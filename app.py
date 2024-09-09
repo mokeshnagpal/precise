81afe56dea3ce997cf4b3aeb46150f4226cd2fb8
Mokesh Nagpal <98022916+mokeshnagpal@users.noreply.github.com>
2024-09-09 21:05:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ Session(app)  # Initialize Flask-Session
 
 # Firebase initialization
 # Load Firebase key content from the environment variable
+
 firebase_key_json = getenv('FIREBASE_KEY')
+print(firebase_key_json)  # Debugging to check if the key is loaded
 
 key_dict = loads(firebase_key_json)
 cred = credentials.Certificate(key_dict)
